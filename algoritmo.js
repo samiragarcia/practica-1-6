2b55ea14afa227f0375052c463af1f8e6b5bd6e1
samira garcia <samira.garcia95@gmail.com>
2023-03-31 18:49:52 -0300

    segunda parte

--- algoritmo.js
+++ algoritmo.js
@@ -1,5 +1,15 @@
-let colorfavorito = prompt("cual es tu color favorito?");
+let numeroEntero = prompt("ingresar el tipo de dato integer/entero:");
+let booleano = prompt("Ingresar el tipo de dato booleano:");
+let numeroDecimal = prompt("ingresar un numero decimal:");
+let caracter = prompt("Ingresa un caracter");
+let palabra = prompt("Ingresar una palabra");
 
-console.log( colorfavorito );
+document.write("<h1> Entero: "+ numeroEntero + "</h1>")
+document.write("<h2> Booleano: "+ booleano + "</h2>");
+document.write("<h3> Decimal: "+ numeroDecimal + "</h3>");
+document.write("<h4> caracter: "+ caracter + "</h4>"); 
+document.write("<h5> Palabra(conjunto de caracteres): "+ palabra + "</h5>");
+
+/* console.log( colorfavorito );
 document.querySelector("body").innerHTML = "<h1> "+ colorfavorito + " </h1>";
-document.write("<h2> "+ colorfavorito + " </h2>");
+document.write("<h2> "+ colorfavorito + " </h2>"); */
