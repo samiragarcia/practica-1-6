6324451505d6b885a4d3be7fe4ccc1d72c334a4b
samira garcia <samira.garcia95@gmail.com>
2023-03-31 17:58:54 -0300

    primer parte 1-6 terminado

---
+++ algoritmo.js
@@ -0,0 +1,5 @@
+let colorfavorito = prompt("cual es tu color favorito?");
+
+console.log( colorfavorito );
+document.querySelector("body").innerHTML = "<h1> "+ colorfavorito + " </h1>";
+document.write("<h2> "+ colorfavorito + " </h2>");
